ec4de6e199264d732a4a5ae46a14376300ab30db
Eddy Moulton <eddy@moulton.com.au>
2020-08-19 20:57:17 +1000

    version 0.1.1

--- build/release.sh
+++ build/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-script_directory=$(dirname ${BASH_SOURCE[0]})
+script_directory=$(dirname $(realpath -s $0))
 src_directory=$(dirname "$script_directory")
 
 source $script_directory/common.sh
@@ -24,13 +24,13 @@ fi
 cd $src_directory
 
 # Increment version
-current_version=$(cat VERSION)
-echo $current_version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' >VERSION
-version=$(cat VERSION)
+current_version=$(cat build/VERSION)
+echo $current_version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' >build/VERSION
+version=$(cat build/VERSION)
 echo "version: $version"
 
 # Run build
-build.sh --environment "$env" --target "$target"
+./build/build.sh --environment "$env" --target "$target"
 
 # Tag in git
 git add -A
@@ -38,19 +38,19 @@ git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 
 # Push to remote - disabled due to permissions
-#git push
-#git push --tags
+git push
+git push --tags
 
-#if [ "$target" = "all" ] || [ "$target" = "api" ]; then
-#  docker tag $REGISTRY/$IMAGE_API:latest $REGISTRY/$IMAGE_API:$version
-#  docker push $REGISTRY/$IMAGE_API:latest
-#  docker push $REGISTRY/$IMAGE_API:$version
-#fi
+if [ "$target" = "all" ] || [ "$target" = "api" ]; then
+  docker tag $REGISTRY/$IMAGE_API:latest $REGISTRY/$IMAGE_API:$version
+  docker push $REGISTRY/$IMAGE_API:latest
+  docker push $REGISTRY/$IMAGE_API:$version
+fi
 
-#if [ "$target" = "all" ] || [ "$target" = "web" ]; then
-#  docker tag $REGISTRY/$IMAGE_WEB:latest $REGISTRY/$IMAGE_WEB:$version
-#  docker push $REGISTRY/$IMAGE_WEB:latest
-#  docker push $REGISTRY/$IMAGE_WEB:$version
-#fi
+if [ "$target" = "all" ] || [ "$target" = "web" ]; then
+  docker tag $REGISTRY/$IMAGE_WEB:latest $REGISTRY/$IMAGE_WEB:$version
+  docker push $REGISTRY/$IMAGE_WEB:latest
+  docker push $REGISTRY/$IMAGE_WEB:$version
+fi
 
 cd -
