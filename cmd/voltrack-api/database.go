2bae18ca719c16a2aafe66b0595e7dc0ae7e9aba
Eddy Moulton <eddy@moulton.com.au>
2020-08-19 20:42:17 +1000

    Change from MySQL to Postgresql

--- cmd/voltrack-api/database.go
+++ cmd/voltrack-api/database.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/eddymoulton/voltrack/pkg/reporting"
 	"github.com/eddymoulton/voltrack/pkg/stocks"
 	"github.com/eddymoulton/voltrack/pkg/transactions"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // InitializeDatabase opens SQLite DB and migrates tables
 func InitializeDatabase() *gorm.DB {
-	db, err := gorm.Open("mysql", "test_user:password@(10.1.1.11)/voltrack_dev?charset=utf8&parseTime=True&loc=Local")
+	dbHost, exists := os.LookupEnv("DB_HOST")
+	if !exists {
+		dbHost = "localhost"
+	}
+
+	dbPort, exists := os.LookupEnv("DB_PORT")
+	if !exists {
+		dbPort = "5432"
+	}
+
+	dbUsername, exists := os.LookupEnv("DB_USER")
+	if !exists {
+		dbUsername = "postgres"
+	}
+
+	dbName, exists := os.LookupEnv("DB_NAME")
+	if !exists {
+		dbName = "postgres"
+	}
+
+	dbPassword, exists := os.LookupEnv("DB_PASSWORD")
+	if !exists {
+		dbPassword = "password"
+	}
+
+	devMode := ""
+	_, exists = os.LookupEnv("ENV_DEVELOPMENT")
+	if exists {
+		devMode = "sslmode=disable"
+	}
+
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s %s", dbHost, dbPort, dbUsername, dbName, dbPassword, devMode))
 
 	if err != nil {
 		log.Fatal(err)
